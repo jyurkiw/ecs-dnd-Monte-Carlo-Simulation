89136e4c6d47d0e37d87e0e52c32499400be3937
Jeffrey Yurkiw <jyurkiw@yahoo.com>
2021-05-04 14:36:50 -0700

    Added rerollBelowThreshold test to roll_test.py

--- tests/roll_test.py
+++ tests/roll_test.py
@@ -2,16 +2,25 @@ from ecs.lib.entity import Entity
 from ecs.lib.entity_manager import EntityManager
 from mcSim.systems.roll import RollSystem
 from mcSim.components.dice import DieCode
+from mcSim.components.dice import RerollBelowThreshold
 from random import randint
 import unittest
 import unittest.mock as mock
 
+def grand():
+    for i in range(100):
+        yield i
+
+grandit = grand()
+
 def nrand(a, b):
-    return 3
+    return next(grandit)
 
 class TestRoll(unittest.TestCase):
     def tearDown(self):
         EntityManager.clear()
+        global grandit
+        grandit = grand()
 
     def test_rollSystem_addsRollComponent(self):
         e = Entity()
@@ -38,9 +47,17 @@ class TestRoll(unittest.TestCase):
 
         with mock.patch('mcSim.systems.roll.randint', nrand):
             rs.run()
-            self.assertEqual(e['Roll'].value, [3,3,3])
+            self.assertEqual(e['Roll'].value, [0,1,2])
 
-    # TODO: Test RerollBelowThreshold component
+    def test_rollSystem_rerollBelowThreshold(self):
+        e = Entity()
+        e.addComponent(DieCode(5, 6))
+        e.addComponent(RerollBelowThreshold(3, 5))
+        rs = RollSystem()
 
+        with mock.patch('mcSim.systems.roll.randint', nrand):
+            rs.run()
+            self.assertEqual(e['Roll'].value, [5,6,7,3,4])
+    
 if __name__ == "__main__":
     unittest.main()
